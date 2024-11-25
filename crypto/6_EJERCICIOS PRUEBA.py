0a1fb954666397418c3131e923a3de06c680ec42
jramale988 <josemanuelramirezalegre@gmail.com>
2024-11-25 12:57:23 +0100

    intento algoritmo

--- crypto/6_EJERCICIOS PRUEBA.py
+++ crypto/6_EJERCICIOS PRUEBA.py
@@ -1,11 +1,11 @@
 lista  = [1,5,2,7,9,32]
 
 n = len(lista)
-
+listaordenada = []
 for posicion in range (n):
 
     for j in range (0, n - posicion - 1):
         if lista[j] < lista[j + 1]:
-            lista [j], lista[j + 1] = lista [j + 1], lista [j]
+            listaordenada.append (lista[j+1])       
 
-print(lista)
+print(listaordenada)
